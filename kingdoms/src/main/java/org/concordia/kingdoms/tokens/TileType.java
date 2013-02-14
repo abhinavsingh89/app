7a6bef1369c80e001dc936371532b49462002f34
vikramwankhede <vikram.vankhede@gmail.com>
2013-02-14 11:59:06 -0500

    Class file in token package with comments

--- kingdoms/src/main/java/org/concordia/kingdoms/tokens/TileType.java
+++ kingdoms/src/main/java/org/concordia/kingdoms/tokens/TileType.java
@@ -1,5 +1,11 @@
 package org.concordia.kingdoms.tokens;
-
+/**
+ * Declared Enum for all types of tiles 
+ * The value of these tiles is within the range - 0 to 5
+ * @author PVRA
+ * @version 1.0-SNAPSHOT
+ *
+ */
 public enum TileType {
 
 	RESOURCE, HAZARD, DRAGON, MOUNTAIN, GOLDMINE, WIZARD;
