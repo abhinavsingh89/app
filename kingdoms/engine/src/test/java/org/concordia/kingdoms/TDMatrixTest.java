96d4f0788dea6b0a6ea9e09308649099a49a9be3
ravindrapotlur <potluru.ravindranath@gmail.com>
2013-03-13 21:31:29 -0400

    Test Cases Added

--- kingdoms/engine/src/test/java/org/concordia/kingdoms/TDMatrixTest.java
+++ kingdoms/engine/src/test/java/org/concordia/kingdoms/TDMatrixTest.java
@@ -17,9 +17,9 @@ public class TDMatrixTest extends TestCase {
 	public void testScore() {
 		TDMatrix matrix = new TDMatrix(5, 6);
 		try {
-			matrix.putComponent(Tile.newTile(TileType.RESOURCE, "test", 6),
+			matrix.putComponent(Tile.newTile(TileType.RESOURCE, "test", 1),
 					TDCoordinate.newInstance(0, 0));
-			matrix.putComponent(Tile.newTile(TileType.HAZARD, "hazard", -1),
+			matrix.putComponent(Tile.newTile(TileType.HAZARD, "hazard", -6),
 					TDCoordinate.newInstance(0, 1));
 			matrix.putComponent(
 					Tile.newTile(TileType.MOUNTAIN, "mountain", null),
@@ -30,17 +30,82 @@ public class TDMatrixTest extends TestCase {
 			matrix.putComponent(Tile.newTile(TileType.HAZARD, "hazard", -5),
 					TDCoordinate.newInstance(0, 4));
 			matrix.putComponent(
-					Tile.newTile(TileType.GOLDMINE, "goldmine", null),
+					Tile.newTile(TileType.DRAGON, "dragon", null),
 					TDCoordinate.newInstance(0, 5));
 
 			List<Integer> scores = Lists.newArrayList();
 			matrix.getScore(0, 6, 0, true, scores);
-			for (Integer score : scores) {
-				System.out.println(score);
-			}
+			assertEquals(Integer.valueOf(-5),scores.get(0));
+			assertEquals(Integer.valueOf(-5),scores.get(1));
+			
 		} catch (GameRuleException e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
+public void testTwoMountainsDragon()
+{
+	TDMatrix matrix = new TDMatrix(5, 6);
+	try {
+		matrix.putComponent(Tile.newTile(TileType.RESOURCE, "test", 1),
+				TDCoordinate.newInstance(0, 0));
+		matrix.putComponent(Tile.newTile(TileType.HAZARD, "hazard", -6),
+				TDCoordinate.newInstance(0, 1));
+		matrix.putComponent(
+				Tile.newTile(TileType.MOUNTAIN, "mountain", null),
+				TDCoordinate.newInstance(0, 2));
+
+		matrix.putComponent(Tile.newTile(TileType.RESOURCE, "test", 1),
+				TDCoordinate.newInstance(0, 3));
+		matrix.putComponent(Tile.newTile(TileType.MOUNTAIN, "mountain",null),
+				TDCoordinate.newInstance(0, 4));
+		matrix.putComponent(
+				Tile.newTile(TileType.DRAGON, "dragon", null),
+				TDCoordinate.newInstance(0, 5));
+
+		List<Integer> scores = Lists.newArrayList();
+		matrix.getScore(0, 6, 0, true, scores);
+		assertEquals(Integer.valueOf(-5),scores.get(0));
+		assertEquals(Integer.valueOf(1),scores.get(1));
+		assertEquals(Integer.valueOf(0),scores.get(2));
+			} catch (GameRuleException e) {
+		e.printStackTrace();
+		fail();
+	}
+}
+public void testMRHRDM()
+{
+
+	TDMatrix matrix = new TDMatrix(5, 6);
+	try {
+		matrix.putComponent(Tile.newTile(TileType.MOUNTAIN, "mountain", null),
+				TDCoordinate.newInstance(0, 0));
+		matrix.putComponent(Tile.newTile(TileType.RESOURCE, "resource", 1),
+				TDCoordinate.newInstance(0, 1));
+		matrix.putComponent(
+				Tile.newTile(TileType.HAZARD, "hazard", -5),
+				TDCoordinate.newInstance(0, 2));
+
+		matrix.putComponent(Tile.newTile(TileType.RESOURCE, "test", 6),
+				TDCoordinate.newInstance(0, 3));
+		matrix.putComponent(Tile.newTile(TileType.DRAGON, "dragon",null),
+				TDCoordinate.newInstance(0, 4));
+		matrix.putComponent(
+				Tile.newTile(TileType.MOUNTAIN, "mountain", null),
+				TDCoordinate.newInstance(0, 5));
+
+		List<Integer> scores = Lists.newArrayList();
+		matrix.getScore(0, 6, 0, true, scores);
+		assertEquals(Integer.valueOf(0),scores.get(0));
+		assertEquals(Integer.valueOf(-5),scores.get(1));
+		assertEquals(Integer.valueOf(0),scores.get(2));
+		for (Integer score : scores) {
+			System.out.println(score);
+		}
+	} catch (GameRuleException e) {
+		e.printStackTrace();
+		fail();
+	}
+
+}
 }
